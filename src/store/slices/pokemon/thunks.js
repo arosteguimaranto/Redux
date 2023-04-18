410a3b13a0d3cb324bdff2878d68d90f2b9a2db9
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-04-18 13:12:52 -0600

    Axios

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,12 +1,18 @@
-import { startLoadingPokemons } from "./pokemonSlice"
+import { pokemonApi } from "../../../api/pokemonApi";
+import {  setPokemons ,startLoadingPokemons } from "./pokemonSlice"
 
 export const getPokemons = (page = 0) => {
     return async(dispatch, getState) => {
         dispatch(startLoadingPokemons());
 
         //TODO: realizar peticion http
+        const resp =  await pokemonApi.get(`pokemon?limit=10&offset=${page * 10 }`)
+        console.log(resp);
         
-        //dispatch(setPokemons());
+        //data-pokemons
+
+
+        dispatch(setPokemons({pokemons: DataTransfer.results, page: page + 1}));
 
     }
 }
